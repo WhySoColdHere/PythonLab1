d53de8dabd87c4c82d94dcae42e2c1474dc41eb5
WhySoColdHere <lavrfan2005@mail.ru>
2025-03-11 00:30:11 +0300

    Task1_task3_commit

--- Task1/common_funcs.py
+++ Task1/common_funcs.py
@@ -1,4 +1,7 @@
 def is_simple(number):
+    number = abs(number)
+    if number < 2:
+        return False
     for sub_number in range(2, int(number ** 0.5) + 1):
         if number % sub_number == 0:
             return False
---
+++ Task1/task3.py
@@ -0,0 +1,21 @@
+from common_funcs import is_simple
+
+
+def func3(n):
+    def is_mutually_simple(num_1, num_2):
+        while num_2:  # Алгоритм евклида
+            num_1, num_2 = num_2, num_1 % num_2
+        return num_1 == 1
+
+    simple_nums_sum = sum((map(lambda x: int(x), filter(lambda x: is_simple(int(x)), str(n)))))
+
+    counter = 0
+    for sub_n in range(1, n):
+        if n % sub_n != 0 and not is_mutually_simple(sub_n, n) and is_mutually_simple(sub_n, simple_nums_sum):
+            counter += 1
+
+    return counter
+
+
+
+
